1de5376de3893a5d38af2506e4292a6994a224c4
Square <squ.devops@gmail.com>
2023-12-10 19:54:27 +0200

    AnsibleSetup.sh impruvements

--- scripts/AnsibleSetup.sh
+++ scripts/AnsibleSetup.sh
@@ -4,6 +4,9 @@
 server_hostname=ansible-server
 myuser=ansadmin
 user_password=1234
+key_name=mykey
+
+sudo apt update
 
 #Changing hostname
 cat << EOF > /etc/hostname
@@ -14,6 +17,8 @@ EOF
 if grep -q 'Ubuntu\|Debian' /etc/issue; then
     sudo adduser ${myuser} --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
     echo "${myuser}:${user_password}" | sudo chpasswd
+    sudo apt install ansible
+    ssh-keygen -t rsa -N "" -f ${key_name}.key
 else   
     sudo useradd ${myuser} --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
     echo "${myuser}:${user_password}" | sudo chpasswd
